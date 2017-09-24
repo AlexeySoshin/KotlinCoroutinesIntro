76f94d404ca751c306578709bf21474b49655755
Alexey Soshin <alexey.soshin@gettaxi.com>
2017-09-24 14:01:18 +0300

    Add select on coroutine example

---
+++ src/main/kotlin/me/soshin/6_SelectOnCoroutine.kt
@@ -0,0 +1,37 @@
+package me.soshin
+
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.channels.produce
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.runBlocking
+import kotlinx.coroutines.experimental.selects.select
+import java.util.*
+
+
+fun main(args: Array<String>) = runBlocking {
+
+
+    val cdn1 = produce(CommonPool) {
+        delay(Random().nextInt(1000).toLong())
+
+        send("A")
+    }
+
+    val cdn2 = produce(CommonPool) {
+        delay(Random().nextInt(1000).toLong())
+
+        send("B")
+    }
+
+    val firstResult = select<String> {
+        cdn1.onReceive { result ->
+            result
+        }
+        cdn2.onReceive { result ->
+            result
+        }
+    }
+
+    println("First result was $firstResult")
+
+}
